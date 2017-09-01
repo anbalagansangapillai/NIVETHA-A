d99d5940f58c924c50a6598e888cafd467bf8208
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2017-09-01 14:03:33 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m;
+printf("enter the num");
+scanf("%d",&m);
+if(m>0)
+{
+printf("it is positive);
+}
+else
+{
+printf("it is negative");
+}
+getch();
+}
