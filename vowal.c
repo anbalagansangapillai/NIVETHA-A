7f3c25d931f6a620b3bddb3f010916563d00ce21
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2017-09-01 14:30:21 +0530

    Create vowal.c

---
+++ vowal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+ char a;
+ int LowerVowel, UpperVowel;
+ printf("Enter the charactor:  ");
+ scanf("%c",&a);
+ if (LowerVowel ||UpperVowel)
+ {
+ printf("%c is a vowel.", a);
+ }
+ else
+ {
+ printf("%c is a consonant.", a);
+ }
+ return 0;
+}
