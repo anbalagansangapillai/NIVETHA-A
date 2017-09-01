427ae06a799a2a125fcabd0b30e0f29bda57d809
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2017-09-01 14:56:31 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a1, a2, a3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a1, &a2, &a3);
+
+    if( a1>=a2 && a1>=a3)
+        printf("%d is the largest number.", a1);
+
+    else if (n2>=n1 && n2>=n3)
+        printf("%d is the largest number.", a2);
+
+    else
+        printf("%d is the largest number.", a3);
+
+    return 0;
+}
