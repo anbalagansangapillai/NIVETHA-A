3b27264bb7f4bd11d7c511fc78319085baefc126
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-01-08 11:31:58 +0000

    Create mmmmmm3.c

---
+++ mmmmmm3.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+    void main()
+    {
+    	int n=6;
+    	if(n=!0&&n<=11)
+    	{
+    		printf("1 to 10");
+    	}
+    	else
+    	{
+    		printf("not 1 to 10");
+    	}
+    }
+     
