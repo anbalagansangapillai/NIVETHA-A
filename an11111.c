158fdbea6aef9ec4dd2433d4668a6b6cac2673f0
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-01-19 11:03:49 +0000

    Create an11111.c

---
+++ an11111.c
@@ -0,0 +1,13 @@
+                        #include<stdio.h>
+                        int main()
+                        {
+                        	int a=124;
+                        	if(a!=0)
+                        	{
+                        		a=a%10;
+                        		a=a/10;
+                        		a++;
+ 
+                        	}
+                        	printf("%d",a++);
+                        }
