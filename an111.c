484270dc9f65c9cd2756203fb5ca69d130339ebe
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-01-19 10:45:19 +0000

    Rename an.c to an111.c

---
+++ an111.c
@@ -0,0 +1,19 @@
+        #include<stdio.h>
+        int main()
+        {
+        	int a=3,b=4,c;
+        	c=a*b;
+        	c=c%2;
+        	if(c==0)
+        	{
+        		printf("even");
+        	}
+        	else
+        	{
+        		printf("odd");
+        	}
+     
+     
+     
+        }
+
