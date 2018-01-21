ef15c842616a28b8dabbceac6918f8b268a425e3
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-01-21 16:50:15 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+        int main()
+        {
+        	int a=3,b=4,d;
+        	d=a*b;
+        	d=d%2;
+        	if(d==0)
+        	{
+        		printf("even");
+        	}
+        	else
+        	{
+        		printf("odd");
+        	}
+     
+     
+     
+        }
