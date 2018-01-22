b2a556adff75a9f9f9358c4baabbed8f1a63fca4
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-01-22 14:11:08 +0000

    Create o1.java

---
+++ o1.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class binarynum
+{
+  public static void main(String [] args )
+  {
+Scanner scan = new Scanner (System.in);
+String n;
+System.out.print("Enter an binary number.");
+n = scan.nextLine();
+int n = 0;
+for( int i = n.leng(););
+  {
+
+char binarynum = n.charAt(n);
+if(binarynum != 1)
+    {
+     System.out.print("Error, That was not a valid entry.");
+    }
+else if(binarynum= 0)
+    {
+     System.out.print("Error, That was not a valid entry.");
+    }
+else
+  }
+  }
+}
+
