52b365e8a364f7b8c35cab628b0df96645401bec
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-01-24 13:31:52 +0000

    Create yy.c

---
+++ yy.c
@@ -0,0 +1,13 @@
+    #include<stdio.h>
+    int main()
+    {
+    	int n=3,i=1,j=7;
+    	if((n>1)&&(n<7))
+    	{
+    		printf("yes");
+    	}
+    	else
+    	{
+    		printf("no");
+    	}
+    }
