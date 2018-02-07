0c0b046b9d86b1d83b3699ffc153f58995fcd5f3
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-02-07 16:22:45 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    int n,r,a=0,v;
+    printf("enter the n value");
+    scanf("%d",&n);
+    v=n;
+    while(n>0)
+    {
+        r=n%10;
+        a=a*10+r;
+        n=n/10;
+        
+        
+    }
+    if(v==a)
+    {
+        printf("palindrome");
+        
+    }
+    else
+    {
+    printf("  not palindrome");
+    }
+    
+}
