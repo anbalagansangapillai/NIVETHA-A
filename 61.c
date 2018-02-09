a5c0bd073eb62b07687aa9df04aa337692e78aa9
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-02-09 14:27:24 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    char a[4]={'\0','a','n','b','u'};
+    int d,i;
+    printf("enter");
+    scanf("%d",&d);
+    for(i=0;i<=d;i++)
+    {
+        printf("%c",a[i]);
+        
+    }
+}
