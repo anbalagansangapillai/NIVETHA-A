c9f2976551f1a65dff39d7ad047850e949de2ef6
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-02-12 10:45:29 +0000

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    void main()
+    {
+    	int le=3,he=2,br=2;
+    	int c;
+    	c=le*he*br;
+    	if(c!=0)
+    	{
+    		printf("%d",c);
+    	}
+    	else
+    	{
+    		printf("empty");
+    	}
+    }
