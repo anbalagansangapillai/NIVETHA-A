b67e2933179a7e51227b9f1d1a06b474f4524ce0
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-02-26 16:56:30 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a[20],n,i,k;
+    
+    printf("enter");
+    scanf("%d",&n);
+    
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+     printf("enter");
+     scanf("%d",&k);
+    printf("the no%d",a[k]);
+    
+    
+}
