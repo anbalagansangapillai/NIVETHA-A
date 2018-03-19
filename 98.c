680d71a452955ad6bfbd140c04732b292c0bf633
anbalagansangapillai <31476553+anbalagansangapillai@users.noreply.github.com>
2018-03-19 15:59:23 +0530

    Create 98.c

---
+++ 98.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int k,n[10],i;
+    printf("enter the k value");
+    scanf("%d",&k);
+    printf("enter");
+    for(i=1;i<=k;i++)
+    {
+        scanf("%d",&n);
+    }
+    for(i=1;i<=k;i++)
+    {
+        printf("%d",i);
+    }
+}
